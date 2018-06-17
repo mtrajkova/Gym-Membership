de12fbd9f3d8e46c902b1ee85201638674328dd7
AleksandarStojanovikj <aleksandar_sangi@hotmail.com>
2018-06-17 17:41:22 +0200

    Make LoginRegisterController return JSON and make login method return User

--- backend/src/main/java/com/fitness/capitol/gym/controllers/LoginRegisterController.java
+++ backend/src/main/java/com/fitness/capitol/gym/controllers/LoginRegisterController.java
@@ -7,6 +7,7 @@ import com.fitness.capitol.gym.excpetions.UserParameterNotFoundException;
 import com.fitness.capitol.gym.model.User;
 import com.fitness.capitol.gym.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,16 +17,16 @@ import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 
 @RestController
-@RequestMapping(value = "/")
+@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class LoginRegisterController {
     @Autowired
     private UserService userService;
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public void login(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session) throws InvalidLoginCredentialsException {
+    public User login(@RequestParam("username") String username, @RequestParam("password") String password) throws InvalidLoginCredentialsException {
         User user = userService.findByUsername(username);
         if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
-            session.setAttribute("user", user);
+            return user;
         } else {
             throw new InvalidLoginCredentialsException("Wrong login credentials");
         }
