8cb48021bbfd8304d54c1ec7d2cc73b7d0a88d59
AleksandarStojanovikj <aleksandar_sangi@hotmail.com>
2018-06-18 12:53:12 +0200

    Make UserController search by username, remove unused imports and formatting

--- backend/src/main/java/com/fitness/capitol/gym/controllers/UserController.java
+++ backend/src/main/java/com/fitness/capitol/gym/controllers/UserController.java
@@ -1,15 +1,11 @@
 package com.fitness.capitol.gym.controllers;
 
-import com.fitness.capitol.gym.excpetions.UserAlreadyExistsException;
-import com.fitness.capitol.gym.excpetions.UserParameterNotFoundException;
 import com.fitness.capitol.gym.model.User;
 import com.fitness.capitol.gym.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -25,10 +21,9 @@ public class UserController {
         return userService.findAll();
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public User getUserById(@PathVariable("id") Long id) {
-        User user = userService.findById(id);
-        return user;
+    @RequestMapping(value = "/{username}", method = RequestMethod.GET)
+    public User getUserByUsername(@PathVariable("username") String username) {
+        return userService.findByUsername(username);
     }
 
 
--- backend/src/main/java/com/fitness/capitol/gym/controllers/WorkoutController.java
+++ backend/src/main/java/com/fitness/capitol/gym/controllers/WorkoutController.java
@@ -1,23 +1,16 @@
 package com.fitness.capitol.gym.controllers;
 
-import com.fitness.capitol.gym.excpetions.UserEmptyException;
 import com.fitness.capitol.gym.model.User;
 import com.fitness.capitol.gym.model.Workout;
 import com.fitness.capitol.gym.service.UserService;
 import com.fitness.capitol.gym.service.WorkoutService;
-import org.hibernate.jdbc.Work;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.Transient;
-import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/workouts", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -31,8 +24,8 @@ public class WorkoutController {
     private UserService userService;
 
 
-    @RequestMapping(value = "/{username}",method = RequestMethod.GET)
-    public ResponseEntity findAllWorkoutsByUser(@PathVariable("username") String username)  {
+    @RequestMapping(value = "/{username}", method = RequestMethod.GET)
+    public ResponseEntity findAllWorkoutsByUser(@PathVariable("username") String username) {
         try {
             User user = userService.findByUsername(username);
             return ResponseEntity.status(HttpStatus.OK).body(workoutService.findAllByUser(user));
@@ -45,9 +38,9 @@ public class WorkoutController {
     @RequestMapping(value = "/addWorkout", method = RequestMethod.POST)
     public ResponseEntity addWorkout(@RequestParam("username") String username, @RequestParam("workoutname") String workoutName) {
         User user;
-        try{
+        try {
             user = this.userService.findByUsername(username);
-        }catch (Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
         Workout workout = new Workout();
